ce8992a9f1f4fb4c95a41882218bbda350c78f62
JCWK <cwk8888cwk@gmail.com>
2021-11-17 00:17:48 +0800

    updated20211117

--- products.py
+++ products.py
@@ -1,33 +1,48 @@
 import os
 
-products = []
-if os.path.isfile("products.csv"):
-	print("File is found!")
-	with open("products.csv", "r", encoding = "utf-8") as f:
-		for line in f:
-			if "Product name,Product price" in line:
-				continue
-			name, price = line.strip().split(",")
-			products.append([name, price])
-	print(products)
-else:
-	print("File does not exist!")
+def read_file(filename):
+    products = []
+    with open(filename, "r", encoding = "utf-8") as f:
+        for line in f:
+            if "Product name,Product price" in line:
+                continue
+            name, price = line.strip().split(",")
+            products.append([name, price])
+    print(products)
+    return products
 
-while True:
-	name = input("Please input product name: ")
-	if name == "q":
-		break
-	price = input("Please input product price: ")
-	price = float(price)
-	products.append([name, price])
-# print(products)
+def user_input(products):
+    while True:
+        name = input("Please input product name: ")
+        if name == "q":
+            break
+        price = input("Please input product price: ")
+        price = float(price)
+        products.append([name, price])
+    print(products)
+    return products
 
-print("The file(products.csv) is created. Below are the data you just added. \n")
-print("Product name", "    ", "Product price")
-for p in products:
-    print(p[0], "    ", p[1])
+def print_products(filename, products):
+    print("The file(", filename, ") is created. Below are the data you just added. \n")
+    print("Product name", "    ", "Product price")
+    for p in products:
+        print(p[0], "    ", p[1])
 
-with open ("products.csv", "w") as f:
-	f.write("Product name" + "," + "Product price" + "\n")
-	for p in products:
-		f.write(p[0] + "," + str(p[1]) + "\n")
+def write_file(filename, products):
+    with open (filename, "w") as f:
+        f.write("Product name" + "," + "Product price" + "\n")
+        for p in products:
+            f.write(p[0] + "," + str(p[1]) + "\n")
+
+def main():
+    filename = "products.csv"
+    if os.path.isfile(filename):
+        print("File is found!")
+        products = read_file(filename)
+    else:
+        print("File does not exist!")
+    products = user_input(products)
+    print_products(filename, products)
+    write_file(filename, products)
+
+main()
